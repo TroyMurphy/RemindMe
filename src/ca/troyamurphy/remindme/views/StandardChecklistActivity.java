85670356f6b029a83912abbef0c52b068b104053
Troy <TroyM@jaysonglobal.com>
2014-09-19 14:27:20 -0600

    Initial commit

---
+++ src/ca/troyamurphy/remindme/views/StandardChecklistActivity.java
@@ -0,0 +1,41 @@
+package ca.troyamurphy.remindme.views;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import ca.troyamurphy.remindme.R;
+
+public class StandardChecklistActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_standard_checklist);
+		
+		//populateListView();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.standard_checklist, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	private void populateListView() {
+		
+	}
+}
