a372206d6f9493d5e503d3589c656fd268e105d2
Troy <TroyM@jaysonglobal.com>
2014-09-25 15:38:47 -0600

    App finished

--- gen/ca/troyamurphy/remindme/R.java
+++ gen/ca/troyamurphy/remindme/R.java
@@ -36,19 +36,19 @@ public final class R {
         public static final int state_selector=0x7f02000b;
     }
     public static final class id {
-        public static final int action_settings=0x7f080008;
-        public static final int addChecklistItemAction=0x7f08000b;
+        public static final int action_settings=0x7f080009;
+        public static final int addChecklistItemAction=0x7f08000c;
         public static final int addChecklistItemEditText=0x7f080000;
         public static final int archiveListView=0x7f080001;
-        public static final int emailAllItems=0x7f08000a;
-        public static final int goToArchive=0x7f080009;
+        public static final int emailAllItems=0x7f08000b;
+        public static final int goToArchive=0x7f08000a;
         public static final int goToStandard=0x7f080005;
         public static final int item_checked=0x7f080002;
         public static final int item_name=0x7f080003;
         public static final int menu_archive=0x7f08000d;
-        public static final int menu_delete=0x7f080006;
-        public static final int menu_email=0x7f08000c;
-        public static final int menu_unarchive=0x7f080007;
+        public static final int menu_delete=0x7f080008;
+        public static final int menu_email=0x7f080007;
+        public static final int menu_unarchive=0x7f080006;
         public static final int standardListView=0x7f080004;
     }
     public static final class layout {
